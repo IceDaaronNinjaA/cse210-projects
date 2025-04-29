d97486dfff981715b58506188541b0ba8e8213a0
IceDaaronNinjaA <3amorft@gmail.com>
2025-04-29 11:15:08 -0600

    Added Hello Dilan

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Console.WriteLine("Hello World!");
         Console.WriteLine("Hey how are you?");
+
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Hello Dilan");
+        }
     }
 }
