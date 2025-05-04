cbcaea4cc85ce1a2525dfb79c890e34051b268ab
IceDaaronNinjaA <3amorft@gmail.com>
2025-05-03 23:20:54 -0600

    Prep 4 0.1.0

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string inputUser = "void";
+        int numberFromUser = 0;
+        List<int> numbers;
+        numbers = new List<int>();
+        do{
+        Console.Write("add a number for the list: ");
+        inputUser = Console.ReadLine();
+        numberFromUser = int.Parse(inputUser);
+        if (numberFromUser != 0)
+        {
+            numbers.Add(numberFromUser);
+        }
+        }
+        while (numberFromUser != 0);
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        
     }
 }
