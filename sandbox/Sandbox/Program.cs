0d837e08552bdf8df15e3c89f201130f9e0dca35
IceDaaronNinjaA <3amorft@gmail.com>
2025-05-08 10:31:40 -0600

    Work from class 5/6

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,16 +1,47 @@
 using System;
+using System.IO.Pipes;
+class Circle
+{
+    private double _radius;
+    
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine ("Error, Error. raduse must > 0");
+            return;
+        }
+        _radius = radius;
+    } 
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
         
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hey how are you?");
+        // Console.WriteLine("Hello World!");
 
-        for(int i = 0; i < 20; i++)
-        {
-            Console.WriteLine("Hello Dilan");
-        }
+        // Console.WriteLine("Hey how are you?");
+
+        // for(int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine("Hello Dilan");
+        // }
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+        Console.WriteLine($"{myCircle.GetArea()}");
+        
     }
 }
