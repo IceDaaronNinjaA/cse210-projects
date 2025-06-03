664b695608c2dfed051628b8085d1038aff54534
IceDaaronNinjaA <3amorft@gmail.com>
2025-06-03 11:04:12 -0600

    rest of last

--- Person/Program.cs
+++ Person/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Progran
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("bob");
+    }
+}
