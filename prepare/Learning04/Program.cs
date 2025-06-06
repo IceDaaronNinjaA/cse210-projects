6588602128fe64fadbf919ca120228f3050ad0bd
IceDaaronNinjaA <3amorft@gmail.com>
2025-06-05 23:28:59 -0600

    Fix joke on Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment programming = new Assignment("Who ThirdBase", "BaseBall Bases Tracker");
+        Assignment programming = new Assignment("Who FirstBase", "BaseBall Bases Tracker");
         Console.WriteLine($"{programming.GetSummery()}");
-        AssignmentMath SportMath = new AssignmentMath("Who ThirdBase", "BaseBall Math", "Sports Math", "Problems bases 1-3");
+        AssignmentMath SportMath = new AssignmentMath("Who FirstBase", "BaseBall Math", "Sports Math", "Problems bases 1-3");
         Console.WriteLine($"{SportMath.GetMathSummary()}");
-        AssignmentEnglish English = new AssignmentEnglish("Who ThirdBase", "Presonal Stories", "When is a joke ended?");
+        AssignmentEnglish English = new AssignmentEnglish("Who FirstBase", "Presonal Stories", "When is a joke ended?");
         Console.WriteLine($"{English.GetEnglishSummary()}");
 
     }
